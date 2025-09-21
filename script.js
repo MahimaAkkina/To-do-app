70e32b490c6cce5ab2e694e8f01635d7b6a5c77c
MahimaAkkina <mahimaakkina@gmail.com>
2025-09-21 23:22:01 +0530

    Updated Js file

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function taskfun(){
     if (inp === ""){
         alert("Enter your Task");
     } 
-    else if (array.includes(inp)){
+    else if (ar.includes(inp)){
         alert("Task already exists!");
     } 
     else{
         let ele = document.createElement('li');
         ele.textContent = inp;     // directly set text
         list.appendChild(ele);
-        array.push(inp);           // stores input
+        ar.push(inp);           // stores input
         document.getElementById('task').value = ""; //clear the date in input field
   }
 }
