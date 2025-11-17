61ede91fdbf09be23ec1ab941b413344a314433c
MahimaAkkina <mahimaakkina@gmail.com>
2025-11-17 14:30:54 +0530

    Changed port env for render

--- backend/server.js
+++ backend/server.js
@@ -57,6 +57,7 @@ app.delete("/items/:id", async (req, res)=>{
     res.json({message:"Task deleted"});
 });
 
-app.listen(4000,()=>{
-    console.log("Server running on port 4000");
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
